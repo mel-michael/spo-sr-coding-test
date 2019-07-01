ce8c9482368046f7654d854f9c6f952c8d97bd96
Mel Michael <mel.michael007@gmail.com>
2019-07-01 07:00:28 +0100

    Update calendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,16 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import './calendar.css';
 
-export default function Calendar({ dates }) {
+function Calendar({ dates }) {
+  const [showReminder, setShowReminder] = useState(false);
+  const [cellDetails, setCellDate] = useState({})
   // TODO: Fix empty cell issue
+  const showReminderFn = cellDetails => {
+    setCellDate(cellDetails);
+    return state => {
+      setShowReminder(state);
+    }
+  }
+
   return (
     <Fragment>
       {dates.map((dateRow, index) => {
         const row = dateRow[index] || new Date();
         return (
           <div key={`${row}_${index}`} className="calendar-row">
-            <CalendarRow rowDates={dateRow} />
+            <CalendarRow rowDates={dateRow} setShowReminder={showReminderFn} />
           </div>
         );
       })}
@@ -18,14 +27,20 @@ export default function Calendar({ dates }) {
   );
 }
 
-function CalendarRow({ rowDates }) {
+function CalendarCell({cellDate, showReminder}) {
+  return (
+    <div className="calendar-cell" onClick={() => showReminder({ date: cellDate })(true)}>
+      {cellDate.getDate()}
+    </div>
+  )
+}
+
+function CalendarRow({ rowDates, setShowReminder }) {
   return (
     <Fragment>
-      {rowDates.map(cellDate => (
-        <div key={cellDate.toString()} className="calendar-cell">
-          {cellDate.getDate()}
-        </div>
-      ))}
+      {rowDates.map(cellDate => <CalendarCell key={cellDate.toString()} cellDate={cellDate} showReminder={setShowReminder} />)}
     </Fragment>
   );
 }
+
+export default Calendar;
